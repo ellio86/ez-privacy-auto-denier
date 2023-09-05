8a9dda2922ebeb680c324411b87bd80b1acac38c
ellio86 <elliotk25@gmail.com>
2023-09-05 13:03:16 +0100

    refactor

--- src/ezPrivacyDenier.ts
+++ src/ezPrivacyDenier.ts
@@ -19,34 +19,43 @@ class EzPrivacyDenier {
 
     private denyCookies () {
         this.dom.manageSettingsButton.click();
-        this.updateCheckboxes();
+
         console.log("Removing cookie consent...");
-        this.dom.allCheckboxes.forEach((checkbox: HTMLInputElement) => {
-            checkbox.checked = false;
-        });
+        this.uncheckCheckboxes();
+
         let showVendorsButton =  this.dom.root.querySelector<HTMLButtonElement>("#ez-show-vendors");
         showVendorsButton.click();
-        this.updateCheckboxes();
+
         console.log("Removing vendor consent...");
-        this.dom.allCheckboxes.forEach((checkbox: HTMLInputElement) => {
-            checkbox.checked = false;
-        });
+        this.uncheckCheckboxes();
+
         console.log("Saving cookie preferences...");
+        this.saveCookiePreferences();
+    }
+
+    private updateCheckboxes () {
+        this.dom.allCheckboxes = this.dom.root.querySelectorAll<HTMLInputElement>("input[type='checkbox']");
+    }
+
+    private saveCookiePreferences() {
         let saveSettingsButton = this.dom.root.querySelector<HTMLButtonElement>("#ez-save-settings");
         saveSettingsButton.click();
 
         console.log("Cookie preferences saved.");
     }
 
-    private updateCheckboxes () {
-        this.dom.allCheckboxes = this.dom.root.querySelectorAll<HTMLInputElement>("input[type='checkbox']");
+    private uncheckCheckboxes() {
+        this.updateCheckboxes();
+        this.dom.allCheckboxes.forEach((checkbox: HTMLInputElement) => {
+            checkbox.checked = false;
+        });
     }
 }
 
 $(() => {
     let cookieDialogue = document.querySelector<HTMLDivElement>("#ez-cookie-dialog");
     if(cookieDialogue) {
-        console.log("Cookie dialogue detected");
+        console.log("EZ Cookie dialogue detected.");
         new EzPrivacyDenier(cookieDialogue);
     }
 });
